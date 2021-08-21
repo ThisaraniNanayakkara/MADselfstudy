04d76fecd8a5c6fed9221ce745eed7afd0c6b422
ThisaraniNanayakkara <it20047788@my.sliit.lk>
2021-08-21 15:59:09 +0530

    Initial commit

---
+++ app/src/main/java/com/mad2021/tutorial4/MainActivity.java
@@ -0,0 +1,39 @@
+package com.mad2021.tutorial4;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.VoiceInteractor;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView mimageView;
+    private static final int REQUEST_IMAGE_CAPTURE =101;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mimageView = findViewById(R.id.imageView2);
+    }
+    public void takePicture(View view){
+        Intent imageCapture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(imageCapture.resolveActivity(getPackageManager())!= null){
+            startActivityForResult(imageCapture,REQUEST_IMAGE_CAPTURE);
+        }
+    }
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap)extras.get("data");
+            mimageView.setImageBitmap(imageBitmap);
+        }
+    }
+}
